640692d03b312b21e55acd52c9e7e84505cf4683
Iñaki Retegi <retegi84@gmail.com>
2025-12-12 21:42:45 +0100

    añadir directorio templates y static

--- fondo/settings.py
+++ fondo/settings.py
@@ -48,7 +48,9 @@ ROOT_URLCONF = 'fondo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
